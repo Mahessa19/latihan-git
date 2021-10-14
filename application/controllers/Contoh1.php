900971aecc27ccbdd8d9bd34cc4eb045e143be6f
Mahessa19 <yudamahessaa19@gmail.com>
2021-10-14 20:40:25 +0700

    menambahkan application/

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo"Nama Saya Mahessa Anggi Yuda
+              Saya Lahir di Daerah Ngawi Jawa Timur
+              olah raga yang saya sukai adalah volly";
+            
+    }
+}
